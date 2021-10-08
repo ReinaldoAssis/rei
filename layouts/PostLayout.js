d7cffbbc6585a38e503ca002fa8623408600293b
Reinaldo <randroi75@gmail.com>
2021-10-08 10:42:38 -0300

    Update: slug page has now animations

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -4,6 +4,7 @@ import SectionContainer from '@/components/SectionContainer'
 import { BlogSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
+import { motion } from 'framer-motion'
 
 // const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 const discussUrl = (slug) =>
@@ -20,22 +21,37 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
     <SectionContainer>
       {/* <BlogSeo url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} /> */}
       <article>
-        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+        <motion.div
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3, ease: 'easeOut' }}
+          className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700"
+        >
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
               <dl className="space-y-10">
-                <div>
+                <motion.div
+                  initial={{ y: -80, opacity: 0 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.7, delay: 0.8, ease: 'easeOut' }}
+                  dateTime={date}
+                >
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
+                    <motion.time>
                       {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
+                    </motion.time>
                   </dd>
-                </div>
+                </motion.div>
               </dl>
-              <div>
+              <motion.div
+                initial={{ y: -80, opacity: 0 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, delay: 0.3, ease: 'easeOut' }}
+                dateTime={date}
+              >
                 <PageTitle>{title}</PageTitle>
-              </div>
+              </motion.div>
             </div>
           </header>
           <div
@@ -47,21 +63,41 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               <dd>
                 <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
                   <li className="flex items-center space-x-2">
-                    <img src={siteMetadata.image} alt="avatar" className="w-10 h-10 rounded-full" />
+                    <motion.img
+                      initial={{ x: -80, opacity: 0 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.9, delay: 0.6, ease: 'easeOut' }}
+                      dateTime={date}
+                      src={siteMetadata.image}
+                      alt="avatar"
+                      className="w-10 h-10 rounded-full"
+                    />
                     <dl className="text-sm font-medium leading-5 whitespace-nowrap">
                       <dt className="sr-only">Name</dt>
-                      <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
-                      {typeof siteMetadata.twitter === 'string' && (
+                      <motion.dd
+                        initial={{ x: -80, opacity: 0 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.7, delay: 0.3, ease: 'easeOut' }}
+                        dateTime={date}
+                        className="text-gray-900 dark:text-gray-100"
+                      >
+                        {siteMetadata.author}
+                      </motion.dd>
+                      {typeof siteMetadata.instagram === 'string' && (
                         <>
                           <dt className="sr-only">Instagram</dt>
-                          <dd>
+                          <motion.dd
+                            initial={{ x: -80, opacity: 0 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.7, delay: 0.4, ease: 'easeOut' }}
+                          >
                             <Link
                               href={siteMetadata.instagram}
                               className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
                             >
                               {siteMetadata.instagram.replace('https://instagram.com/', '@')}
                             </Link>
-                          </dd>
+                          </motion.dd>
                         </>
                       )}
                     </dl>
@@ -70,12 +106,19 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
-              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">{children}</div>
+              <motion.div
+                initial={{ x: 80, opacity: 0 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.7, delay: 0.3, ease: 'easeOut' }}
+                className="pt-10 pb-8 prose dark:prose-dark max-w-none"
+              >
+                {children}
+              </motion.div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(slug)} rel="nofollow">
+                {/* <Link href={discussUrl(slug)} rel="nofollow">
                   {'Discuss on Twitter'}
-                </Link>
-                {` • `}
+                </Link> */}
+                {/* {` • `} */}
                 {/* <Link href={editUrl(fileName)}>{'View on GitHub'}</Link> */}
               </div>
             </div>
@@ -83,12 +126,23 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
                 {tags && (
                   <div className="py-4 xl:py-8">
-                    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
+                    <motion.h2
+                      initial={{ y: 80, opacity: 0 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.7, delay: 0.3, ease: 'easeOut' }}
+                      className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400"
+                    >
                       Tags
-                    </h2>
+                    </motion.h2>
                     <div className="flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
+                      {tags.map((tag, i) => (
+                        <motion.div
+                          initial={{ y: 80, opacity: 0 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          transition={{ duration: 0.7, delay: 0.8 + 0.3 * i, ease: 'easeOut' }}
+                        >
+                          <Tag key={tag} text={tag} />
+                        </motion.div>
                       ))}
                     </div>
                   </div>
@@ -128,7 +182,7 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
               </div>
             </footer>
           </div>
-        </div>
+        </motion.div>
       </article>
     </SectionContainer>
   )
