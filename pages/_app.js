04a3987be413fedaeaf4836a947e77cafc1997b1
Reinaldo <randroi75@gmail.com>
2021-10-16 23:11:55 -0300

    beta: dynamic favicon for dark & light theme

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,60 @@ import { AnimateSharedLayout, motion, AnimatePresence } from 'framer-motion'
 import { black, white } from 'tailwindcss/colors'
 
 export default function App({ Component, pageProps }) {
-  const { theme } = useTheme()
+  const { theme, resolvedTheme } = useTheme()
   return (
     <ThemeProvider attribute="class">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        {theme === 'dark' || resolvedTheme === 'dark' ? (
+          <>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/static/favicons/dark/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/static/favicons/dark/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/static/favicons/dark/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/static/favicons/dark/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/static/favicons/dark/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+          </>
+        ) : (
+          <>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/static/favicons/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/static/favicons/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/static/favicons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/static/favicons/site.webmanifest" />
+            <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          </>
+        )}
       </Head>
       <DefaultSeo {...SEO} />
       <LayoutWrapper>
